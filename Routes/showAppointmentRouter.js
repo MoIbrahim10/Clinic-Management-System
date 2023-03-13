4356004a889fbf91750b847467eab5a285b6b340
hassan9810 <eng.hsn10@gmail.com>
2023-03-13 17:51:06 +0200

    Add Weekly Schedule Router  **[/Home]**

--- Routes/showAppointmentRouter.js
+++ Routes/showAppointmentRouter.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const controller = require("../Controller/clinicController");
+const controller = require("../Controller/showAppointmentsController");
 
 const router = express.Router();
 
-
 // to show available appointemnt
-
-
 router
     .route("/home")
-    .get(controller.getAllClinics);
+    .get(controller.getAllWeeklySchedule);
 
 module.exports = router;
 
